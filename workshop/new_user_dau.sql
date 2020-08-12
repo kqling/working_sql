fb387bd02f772ba83f91a64a969dd9bc907ed0e7
Xinyao Ma <mac@MacdeMacBook-Pro-3.local>
2020-08-12 18:13:33 +0800

    Change new_user_dau

--- workshop/new_user_dau.sql
+++ workshop/new_user_dau.sql
@@ -1,22 +1,20 @@
 SELECT
     'Android' as platform,
-    concat(substr(_table_suffix,0,4),'-',substr(_table_suffix,5,2),'-',substr(_table_suffix,7,2)) as date,
-    COUNT(distinct CASE when living_days = 0 then user_pseudo_id else null end) as new_user,
+    format_date('%Y-%m-%d', parse_date("%Y%m%d", _TABLE_SUFFIX)) as date,
+    COUNT(distinct if(living_days = 0, user_pseudo_id, null)) as new_user,
     COUNT(distinct user_pseudo_id) as DAU
-FROM `blockpuzzle-f21e1.bi_data_warehouse.basic_events_android_*`,
-UNNEST(event_params) as evt
-WHERE cast(concat(substr(_table_suffix,0,4),'-',substr(_table_suffix,5,2),'-',substr(_table_suffix,7,2)) as date) between cast('2020-03-01' as date) AND CURRENT_DATE()
+FROM `blockpuzzle-f21e1.bi_data_warehouse.basic_events_android_*`
+WHERE parse_date("%Y%m%d", _TABLE_SUFFIX) between date('2020-07-01') AND CURRENT_DATE()
 AND event_name not in ('act_set_notification','act_receive_notification','os_update','firebase_campaign','first_open','user_engagement')
 GROUP BY 1,2
 UNION ALL
 SELECT
     'iOS' as platform,
-    concat(substr(_table_suffix,0,4),'-',substr(_table_suffix,5,2),'-',substr(_table_suffix,7,2)) as date,
-    COUNT(distinct CASE when living_days = 0 then user_pseudo_id else null end) as new_user,
+    format_date('%Y-%m-%d', parse_date("%Y%m%d", _TABLE_SUFFIX)) as date,
+    COUNT(distinct if(living_days = 0, user_pseudo_id, null)) as new_user,
     COUNT(distinct user_pseudo_id) as DAU
-FROM `blockpuzzle-f21e1.bi_data_warehouse.basic_events_ios_*`,
-UNNEST(event_params) as evt
-WHERE cast(concat(substr(_table_suffix,0,4),'-',substr(_table_suffix,5,2),'-',substr(_table_suffix,7,2)) as date) between cast('2020-03-01' as date) AND CURRENT_DATE()
+FROM `blockpuzzle-f21e1.bi_data_warehouse.basic_events_ios_*`
+WHERE parse_date("%Y%m%d", _TABLE_SUFFIX) between date('2020-07-01') AND CURRENT_DATE()
 AND event_name not in ('act_set_notification','act_receive_notification','os_update','firebase_campaign','first_open','user_engagement')
 GROUP BY 1,2
 ORDER BY 1,2
