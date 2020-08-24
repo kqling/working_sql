3ce79841ddc7cef389c3b6a2a960dba24203cfee
Xinyao Ma <mac@MacdeMacBook-Pro-4.local>
2020-08-24 15:40:12 +0800

    Change taichi project sql with new table and timeline

--- project/taichi/ct_block_puzzle_ios_us.sql
+++ project/taichi/ct_block_puzzle_ios_us.sql
@@ -24,18 +24,18 @@ FROM
                 app_version,
                 ad_id,
                 country,
-                date_add(current_date(), interval -30 day) as date,
+                date_add(current_date(), interval -14 day) as date,
                 SUM(true_show) as total_show
             from `blockpuzzle-f21e1.bi_data_warehouse.adsdk_events_ios_*`
-            where parse_date('%Y%m%d',_table_suffix) between date_add(current_date(), interval -30 day) and date_add(current_date(), interval -10 day)
-            AND country = 'United States'
-            AND DATE_ADD(parse_date('%Y%m%d',_table_suffix),interval 0-living_days DAY) = date_add(current_date(), interval -30 day)
-            and parse_date('%Y%m%d',summary_date) between date_add(current_date(), interval -30 day) and date_add(current_date(), interval -23 day)
+            where parse_date('%Y%m%d',_table_suffix) between date_add(current_date(), interval -14 day) and current_date()
+            AND DATE_ADD(parse_date('%Y%m%d',_table_suffix),interval 0-living_days DAY) = date_add(current_date(), interval -14 day)
+            and parse_date('%Y%m%d',summary_date) between date_add(current_date(), interval -14 day) and date_add(current_date(), interval -7 day)
+            and country = 'United States'
             GROUP BY 1,2,3,4,5,6
-            HAVING total_show > 0) s
-        left join `blockpuzzle-f21e1.bi_data_warehouse.unit_ecpm_ios` e   
-        ON s.summary_date = e.date
+            HAVING total_show > 0 and total_show < 1000000) s
+        left join `blockpuzzle-f21e1.learnings_data_warehouse_ios.fact_dws_iaa_unitEcpm_a` e   
+        ON parse_date('%Y%m%d',s.summary_date) = e.date
         AND s.ad_id = e.unit_id
-        and s.country = e.country
+        and s.country = e.country_name
         GROUP BY 1,2,3))
 WHERE rank_perc <= 0.3
--- project/taichi/ct_pbn_ios_us.sql
+++ project/taichi/ct_pbn_ios_us.sql
@@ -24,18 +24,40 @@ FROM
                 app_version,
                 ad_id,
                 country,
-                date_add(current_date(), interval -30 day) as date,
+                date_add(current_date(), interval -14 day) as date,
                 SUM(true_show) as total_show
             from `paint-by-number-3c789.bi_data_warehouse.adsdk_events_ios_*`
-            where parse_date('%Y%m%d',_table_suffix) between date_add(current_date(), interval -30 day) and date_add(current_date(), interval -10 day)
-            AND country = 'United States'
-            AND DATE_ADD(parse_date('%Y%m%d',_table_suffix),interval 0-living_days DAY) = date_add(current_date(), interval -30 day)
-            and parse_date('%Y%m%d',summary_date) between date_add(current_date(), interval -30 day) and date_add(current_date(), interval -23 day)
+            where parse_date('%Y%m%d',_table_suffix) between date_add(current_date(), interval -14 day) and current_date()
+            AND DATE_ADD(parse_date('%Y%m%d',_table_suffix),interval 0-living_days DAY) = date_add(current_date(), interval -14 day)
+            and parse_date('%Y%m%d',summary_date) between date_add(current_date(), interval -14 day) and date_add(current_date(), interval -7 day)
+            and country = 'United States'
             GROUP BY 1,2,3,4,5,6
             HAVING total_show > 0) s
-        left join `paint-by-number-3c789.bi_data_warehouse.unit_ecpm_ios` e   
-        ON s.summary_date = e.date
+        left join 
+            (select 
+                iaa_platform, 
+                date, 
+                unit_id, 
+                ip_name as country_name, 
+                s1.country_code,
+                ifnull(sum(revenue), 0) as revenue, ifnull(sum(impression), 0) as impression,
+                round(ifnull(safe_divide(sum(revenue), sum(impression)) * 1000, 0), 10) as ecpm
+            from `foradmobapi.learnings_data_warehouse.fact_dwd_iaa_unitRevenue_di_*` s1 
+            join 
+                (select 
+                    k2.app_id, 
+                    k2.platform 
+                from `foradmobapi.learnings_data_warehouse.dim_dwd_iaa_application_a` k2
+                where production_id = '5b892d3a9f9b4e00011d1cf3'
+                ) s2 
+            on s1.app_id = s2.app_id 
+            and ifnull(s1.platform, 'nt') = ifnull(s2.platform, 'nt')
+            left join `foradmobapi.learnings_data_warehouse.dim_dwd_basic_country_a` s3 
+            on s1.country_code = s3.country_code
+            where parse_date('%Y%m%d',_TABLE_SUFFIX) between date_add(current_date(), interval -14 day) and date_add(current_date(), interval -7 day)
+            group by 1, 2, 3, 4, 5) e   
+        ON parse_date('%Y%m%d',s.summary_date) = e.date
         AND s.ad_id = e.unit_id
-        and s.country = e.country
+        and s.country = e.country_name
         GROUP BY 1,2,3))
 WHERE rank_perc <= 0.3
