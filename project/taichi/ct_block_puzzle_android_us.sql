94f9241b5e9a231fe3cfd1ff54d26bb76e7e5a5f
Xinyao Ma <mac@MacdeMacBook-Pro-5.local>
2020-09-26 11:49:27 +0800

    Taichi transfer to db finished

--- project/taichi/ct_block_puzzle_android_us.sql
+++ project/taichi/ct_block_puzzle_android_us.sql
@@ -21,39 +21,33 @@ FROM
         percent_rank() OVER(partition by date ORDER BY six_day_revenue DESC) as six_day_rank_perc
     FROM
         (SELECT
-            s.date,
-            s.user_pseudo_id,
-            app_version,
-            round(sum(ifnull(s.two_day_total_show, 0) * ifnull(e.ecpm, 0) / 1000), 2) as two_day_revenue,
-            round(sum(ifnull(s.four_day_total_show, 0) * ifnull(e.ecpm, 0) / 1000), 2) as four_day_revenue,
-            round(sum(ifnull(s.six_day_total_show, 0) * ifnull(e.ecpm, 0) / 1000), 2) as six_day_revenue
+            u.date,
+            u.user_pseudo_id,
+            u.app_version,
+            ad.two_day_revenue,
+            ad.four_day_revenue,
+            ad.six_day_revenue
         FROM
-            (select 
-                summary_date,
+            (SELECT
+                create_date as date,
                 user_pseudo_id,
-                app_version,
-                ad_id,
-                country,
-                date_add(@run_date, interval -7 day) as date,
-                SUM(case when parse_date('%Y%m%d',summary_date) between date_add(@run_date, interval -7 day)
-                            and date_add(@run_date, interval -6 day)
-                        then true_show else 0 end) as two_day_total_show,
-                SUM(case when parse_date('%Y%m%d',summary_date) between date_add(@run_date, interval -7 day)
-                            and date_add(@run_date, interval -4 day)
-                        then true_show else 0 end) as four_day_total_show,
-                SUM(true_show) as six_day_total_show
-            from `blockpuzzle-f21e1.bi_data_warehouse.adsdk_events_android_*`
-            where parse_date('%Y%m%d',_table_suffix) between date_add(@run_date, interval -7 day) and @run_date
-            --create_date是7天前
-            AND DATE_ADD(parse_date('%Y%m%d',_table_suffix),interval 0-living_days DAY) = date_add(@run_date, interval -7 day) 
-            --广告实际发生的时间是6天内
-            and parse_date('%Y%m%d',summary_date) between date_add(@run_date, interval -7 day) and date_add(@run_date, interval -2 day)
-            and country = 'United States'
-            GROUP BY 1,2,3,4,5,6
-            HAVING two_day_total_show > 0 and two_day_total_show < 1000000) s
-        left join `blockpuzzle-f21e1.learnings_data_warehouse_android.fact_dws_iaa_unitEcpm_a` e   
-        ON parse_date('%Y%m%d',s.summary_date) = e.date
-        AND s.ad_id = e.unit_id
-        and s.country = e.country_name
-        GROUP BY 1,2,3))
+                app_version
+            FROM `blockpuzzle-f21e1.learnings_data_warehouse_android.dim_dwd_action_userInfo_a`
+            WHERE first_day_geo.country = 'United States'
+            AND create_date = DATE_ADD(@run_date, interval -8 day)
+            ) u
+        LEFT JOIN
+            (SELECT
+                user_pseudo_id,
+                SUM(CASE when date between date_add(@run_date, interval -8 day) and date_add(@run_date, interval -7 day) then ad_revenue else 0 end) as two_day_revenue,
+                SUM(CASE when date between date_add(@run_date, interval -8 day) and date_add(@run_date, interval -5 day) then ad_revenue else 0 end) as four_day_revenue,
+                SUM(ad_revenue) as six_day_revenue
+            FROM `blockpuzzle-f21e1.learnings_data_warehouse_android.analytics_dm_action_userPrimaryMetric_di_*`
+            WHERE date between DATE_ADD(@run_date, interval -8 day) and @run_date
+            AND create_date = DATE_ADD(@run_date, interval -8 day)
+            GROUP BY 1
+            ) ad
+        ON u.user_pseudo_id = ad.user_pseudo_id
+        )
+    )
 WHERE (two_day_rank_perc <= 0.3 or four_day_rank_perc <= 0.3 or six_day_rank_perc <= 0.3)
