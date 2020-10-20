97bbdfc41cac30ff9cbb040ecf69dc8c76a4e618
Xinyao Ma <mac@MacdeMacBook-Pro-6.local>
2020-10-20 10:28:06 +0800

    change file structure

---
+++ project/roi/ua_period/ct_roi_everyday_revenue_ios.sql
@@ -0,0 +1,39 @@
+SELECT
+    'iOS' as platform,
+    DATE_SUB(@run_date, INTERVAL 0 DAY) AS run_date,
+    u.user_pseudo_id,
+    u.media_source,
+    u.first_day_country,
+    date_diff(@run_date,u.create_date,day) as living_days,
+    u.create_date,
+    case when a.ad_revenue is not null then a.ad_revenue else 0 end as ad_revenue
+FROM
+    (SELECT
+        distinct a.user_pseudo_id,
+        u.first_day_geo.country as first_day_country,
+        u.media_source,
+        a.create_date
+    FROM `blockpuzzle-f21e1.learnings_data_warehouse_ios.analytics_dm_action_userPrimaryMetric_di_*` a
+    INNER JOIN `blockpuzzle-f21e1.learnings_data_warehouse_ios.dim_dwd_action_userInfo_a` u 
+    ON a.user_pseudo_id = u.user_pseudo_id
+    AND u.first_day_geo.country in ('United States','United Kingdom','Germany','France','Russia','Japan','Brazil','Canada')
+    WHERE a.date between '2020-04-01' and '2020-08-31'
+    AND a.create_date between date('2020-04-01') and @run_date) u
+LEFT JOIN
+    (SELECT
+        a.date,
+        a.user_pseudo_id,
+        a.living_days,
+        a.create_date,
+        SUM(ad_revenue) as ad_revenue
+    FROM `blockpuzzle-f21e1.learnings_data_warehouse_ios.analytics_dm_action_userPrimaryMetric_di_*` a
+    INNER JOIN `blockpuzzle-f21e1.learnings_data_warehouse_ios.dim_dwd_action_userInfo_a` u 
+    ON a.user_pseudo_id = u.user_pseudo_id
+    AND u.first_day_geo.country in ('United States','United Kingdom','Germany','France','Russia','Japan','Brazil','Canada')
+    WHERE a.date between '2020-04-01' and '2020-08-31'
+    AND a.create_date between '2020-04-01' and '2020-08-31'
+    GROUP BY 1,2,3,4) a
+ON a.user_pseudo_id = u.user_pseudo_id
+AND a.date = @run_date
+
+
